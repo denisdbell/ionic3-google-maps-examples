593456a2be8709d2b0691b7e8b98e875f6aa8f94
Denis Bell <denisdbell@gmail.com>
2018-02-05 16:06:50 -0500

    Add sample image for content

--- src/pages/example-2/example-2.ts
+++ src/pages/example-2/example-2.ts
@@ -22,7 +22,7 @@ export class Example_2Page {
                           '<h1 id="firstHeading" class="firstHeading">Sound Love VS Love Sound Disco</h1>'+
                           '<div id="bodyContent">'+
                           '<p>' +
-                          '<img style="-webkit-user-select: none;background-position: 0px 0px, 10px 10px;background-size:' +
+                          '<img width="100px" height="100px" style="-webkit-user-select: none;background-position: 0px 0px, 10px 10px;background-size:' +
                           '20px 20px;background-image:linear-gradient(45deg, #eee 25%, transparent 25%, transparent 75%, #eee 75%, #eee 100%),linear-gradient(45deg, #eee 25%, white 25%, white 75%, #eee 75%, #eee 100%);" src="https://thumbnailer.mixcloud.com/unsafe/300x300/extaudio/d/0/8/f/6790-4721-477c-b467-973790ca3423.jpg">' +
                           '<p>'+
                           '</div>'+
@@ -42,7 +42,193 @@ export class Example_2Page {
     this.map = new google.maps.Map(this.mapElement.nativeElement, {
       zoom: 9,
       center: posMaceio,
-      mapTypeId: 'roadmap'
+      mapTypeId: 'roadmap',
+      styles: [
+        {
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#212121"
+            }
+          ]
+        },
+        {
+          "elementType": "labels.icon",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#757575"
+            }
+          ]
+        },
+        {
+          "elementType": "labels.text.stroke",
+          "stylers": [
+            {
+              "color": "#212121"
+            }
+          ]
+        },
+        {
+          "featureType": "administrative",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#757575"
+            }
+          ]
+        },
+        {
+          "featureType": "administrative.country",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#9e9e9e"
+            }
+          ]
+        },
+        {
+          "featureType": "administrative.land_parcel",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "administrative.locality",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#bdbdbd"
+            }
+          ]
+        },
+        {
+          "featureType": "poi",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#757575"
+            }
+          ]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#181818"
+            }
+          ]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#616161"
+            }
+          ]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "labels.text.stroke",
+          "stylers": [
+            {
+              "color": "#1b1b1b"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "geometry.fill",
+          "stylers": [
+            {
+              "color": "#2c2c2c"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#8a8a8a"
+            }
+          ]
+        },
+        {
+          "featureType": "road.arterial",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#373737"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#3c3c3c"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway.controlled_access",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#4e4e4e"
+            }
+          ]
+        },
+        {
+          "featureType": "road.local",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#616161"
+            }
+          ]
+        },
+        {
+          "featureType": "transit",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#757575"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#FFFFFF"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#3d3d3d"
+            }
+          ]
+        }
+      ]
     });
     
     google.maps.event.addListener(this.map, 'click', (event) => {
@@ -62,7 +248,8 @@ export class Example_2Page {
     let marker = new google.maps.Marker({
       position: location,
       label: this.labels[this.labelIndex++ % this.labels.length],
-      map: map
+      map: map,
+      icon: '../assets/icon/guinness.png'
     });
 
     google.maps.event.addListener(marker, 'click', (event) => {
