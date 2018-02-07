97a7314507eed0eab221208f011be8ed5fae27ac
Denis Bell <denisdbell@gmail.com>
2018-02-07 15:13:18 -0500

    Add additional mock data

--- src/app/services/mock-data.ts
+++ src/app/services/mock-data.ts
@@ -3,17 +3,35 @@ import { MapMarker } from '../models/MapMarker';
 
 export const MAPMARKERS: MapMarker[] = [
     {  
-        label: "test",
+        label: "clash_loser",
         icon: "guinness.png",
         description: "<h1>test</h1>",
+        category: "clash_loser",
         latitude: 18.1096, 
-        longitude: -77.2975
+        longitude: -77.100
     },
     {  
-        label: "test1",
+        label: "clash_winner",
         icon: "guinness.png",
         description: "<h1>test1</h1>",
+        category: "clash_winner",
         latitude: 18.1096, 
-        longitude: -78.2975
+        longitude: -77.200
+    },
+    {  
+        label: "promotion",
+        icon: "guinness.png",
+        description: "<h1>test1</h1>",
+        category: "promotion",
+        latitude: 18.1096, 
+        longitude: -77.300
+    },
+    {  
+        label: "higlight",
+        icon: "guinness.png",
+        description: "<h1>test1</h1>",
+        category: "higlight",
+        latitude: 18.1096, 
+        longitude: -77.500
     }
 ]
