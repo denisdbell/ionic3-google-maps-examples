c7bd9446ecb01a00e721004c8514b94349324b10
Denis Bell <denisdbell@gmail.com>
2018-02-07 12:22:19 -0500

    Add mock data

---
+++ src/app/services/mock-data.ts
@@ -0,0 +1,19 @@
+import { MapMarker } from '../models/MapMarker';
+
+
+export const MAPMARKERS: MapMarker[] = [
+    {  
+        label: "test",
+        icon: "guinness.png",
+        description: "test",
+        latitude: 18.1096, 
+        longitude: -77.2975
+    },
+    {  
+        label: "test1",
+        icon: "guinness.png",
+        description: "test1",
+        latitude: 18.1096, 
+        longitude: -78.2975
+    }
+]
