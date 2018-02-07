ccb590ee62fc4be6643513787c447de7c7d43524
Denis Bell <denisdbell@gmail.com>
2018-02-07 15:13:08 -0500

    Add category

--- src/app/models/MapMarker.ts
+++ src/app/models/MapMarker.ts
@@ -2,6 +2,7 @@ export class MapMarker {
     public label: string;
     public icon: string;
     public description: string;
+    public category: string;
     public latitude: number;
     public longitude: number;
 }
