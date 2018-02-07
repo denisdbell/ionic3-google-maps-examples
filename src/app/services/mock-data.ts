3e68bb9184b92dfba3910fbb7f6e54a76164e6fe
Denis Bell <denisdbell@gmail.com>
2018-02-07 12:51:10 -0500

    Update description in mock data to include html

--- src/app/services/mock-data.ts
+++ src/app/services/mock-data.ts
@@ -5,14 +5,14 @@ export const MAPMARKERS: MapMarker[] = [
     {  
         label: "test",
         icon: "guinness.png",
-        description: "test",
+        description: "<h1>test</h1>",
         latitude: 18.1096, 
         longitude: -77.2975
     },
     {  
         label: "test1",
         icon: "guinness.png",
-        description: "test1",
+        description: "<h1>test1</h1>",
         latitude: 18.1096, 
         longitude: -78.2975
     }
