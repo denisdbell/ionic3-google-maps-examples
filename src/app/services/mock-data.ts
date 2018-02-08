494522ec2ffd3ad913f3a3c01213236251a8ac4d
Denis Bell <denisdbell@gmail.com>
2018-02-08 17:00:26 -0500

    Update mock data

--- src/app/services/mock-data.ts
+++ src/app/services/mock-data.ts
@@ -9,7 +9,7 @@ Latitude: 18.179778291829212 , longitude: -77.34375
 Latitude: 18.025751281356246 , longitude: -77.662353515625
 Latitude: 18.140631722312712 , longitude: -77.6239013671875
 Latitude: 18.291949733550336 , longitude: -77.85736083984375
-Latitude: 18.187606552494625 , longitude: -77.64862060546875
+Latitude: 18.187606552494625 , longitude: -77.64862060546875 -
 Latitude: 18.427501971948608 , longitude: -77.59918212890625
 Latitude: 18.375379094031825 , longitude: -78.08807373046875
 Latitude: 18.359739156553683 , longitude: -78.18695068359375 -
@@ -29,6 +29,14 @@ export const MAPMARKERS: MapMarker[] = [
         category: "clash_loser",
         latitude: 17.929089201618645, 
         longitude: -76.5802001953125
+    },
+    {  
+        label: "clash_loser",
+        icon: "loser.png",
+        description: "<h3>Stone Love loses to Mighty Crown</h3>",
+        category: "clash_loser",
+        latitude: 18.187606552494625, 
+        longitude: -77.64862060546875
     },
     {  
         label: "clash_loser",
@@ -55,20 +63,53 @@ export const MAPMARKERS: MapMarker[] = [
         longitude: -76.3275146484375
     },
     {  
+        label: "clash_winner",
+        icon: "winner.png",
+        description: "<h3>Optimus Music Beats Emperor Sound System</h3>",
+        category: "clash_winner",
+        latitude:   18.028363034996826, 
+        longitude: -76.42913818359375
+    },
+    {  
+        label: "clash_winner",
+        icon: "winner.png",
+        description: "<h3>Optimus Music Beats Emperor Sound System</h3>",
+        category: "clash_winner",
+        latitude:  17.99440721713975, 
+        longitude: -76.58843994140625
+    },
+    
+    {  
         label: "promotion",
         icon: "promotion.png",
         description: "<h3>Free Beer</h3>",
         category: "promotion",
-        latitude: 7.855904414319152, 
-        longitude:-77.4481201171875
+        latitude: 17.855904414319152, 
+        longitude: -77.4481201171875
     },
     {  
         label: "promotion",
         icon: "promotion.png",
         description: "<h3>Free T-Shirts</h3>",
         category: "promotion",
-        latitude: 17.72514243272563, 
-        longitude: -77.18170166015625
+        latitude: 18.375379094031825 , 
+        longitude: -78.08807373046875
+    },
+    -{  
+        label: "promotion",
+        icon: "promotion.png",
+        description: "<h3>Free T-Shirts</h3>",
+        category: "promotion",
+        latitude: 18.375379094031825 , 
+        longitude: -78.08807373046875
+    },
+    {  
+        label: "promotion",
+        icon: "promotion.png",
+        description: "<h3>Free T-Shirts</h3>",
+        category: "promotion",
+        latitude: 18.375379094031825 , 
+        longitude: -78.08807373046875
     },
     {  
         label: "higlight",
@@ -84,7 +125,7 @@ export const MAPMARKERS: MapMarker[] = [
         description: "<h3>Stone Love Vs Mighty Crown</h3>",
         category: "highlight",
         latitude: 17.897727988564345, 
-        longitude: 77.574462890625
+        longitude: -77.574462890625
     },
     {  
         label: "higlight",
