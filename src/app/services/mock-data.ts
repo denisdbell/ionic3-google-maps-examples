85b465a897459d42f906129e45f0c0c248469ae1
Denis Bell <denisdbell@gmail.com>
2018-02-12 09:54:14 -0500

    Update to mock data to include more winners and social sharing icon

--- src/app/services/mock-data.ts
+++ src/app/services/mock-data.ts
@@ -1,48 +1,124 @@
 import { MapMarker } from '../models/MapMarker';
 
-/*Sample lat lang
-Latitude: 18.028363034996826 , longitude: -76.42913818359375
-Latitude: 17.99440721713975 , longitude: -76.58843994140625
-Latitude: 18.046644225282463 , longitude: -77.0855712890625
-Latitude: 18.294557510034192 , longitude: -77.1240234375
-Latitude: 18.179778291829212 , longitude: -77.34375
-Latitude: 18.025751281356246 , longitude: -77.662353515625
-Latitude: 18.140631722312712 , longitude: -77.6239013671875
-Latitude: 18.291949733550336 , longitude: -77.85736083984375
-Latitude: 18.187606552494625 , longitude: -77.64862060546875 -
-Latitude: 18.427501971948608 , longitude: -77.59918212890625
-Latitude: 18.375379094031825 , longitude: -78.08807373046875
-Latitude: 18.359739156553683 , longitude: -78.18695068359375 -
-Latitude: 18.276302250871954 , longitude: -78.13751220703125
-Latitude: 17.897727988564345 , longitude: -77.574462890625
-Latitude: 17.816685878972844 , longitude: -77.266845703125
-Latitude: 17.72514243272563 , longitude: -77.18170166015625
-Latitude: 17.855904414319152 , longitude: -77.4481201171875
-Latitude: 17.95260646769184 , longitude: -76.3275146484375
-Latitude: 17.929089201618645 , longitude: -76.5802001953125
-*/
 export const MAPMARKERS: MapMarker[] = [
-    {  
-        label: "clash_loser",
-        icon: "loser.png",
+    
+     {  
+        label: "clash_winner",
+        icon: "winner.png",
         description: "<h3>Stone Love loses to Mighty Crown</h3>",
-        category: "clash_loser",
+        category: "clash_winner",
+        latitude:  18.252828380738396 , 
+        longitude:  -77.83676147460938
+     },
+     {  
+        label: "clash_winner",
+        icon: "winner.png",
+        description: "<h3>Stone Love loses to Mighty Crown</h3>",
+        category: "clash_winner",
+        latitude:  18.40926075928673 , 
+        longitude: -77.83676147460938
+    },
+    {  
+        label: "clash_winner",
+        icon: "winner.png",
+        description: "<h3>Stone Love loses to Mighty Crown</h3>",
+        category: "clash_winner",
+        latitude:  18.071451372294696  , 
+        longitude: -77.75161743164062
+    },
+    {  
+        label: "clash_winner",
+        icon: "winner.png",
+        description: "<h3>Stone Love loses to Mighty Crown</h3>",
+        category: "clash_winner",
+        latitude:  18.04533848897631    , 
+        longitude: -77.30804443359375
+    },
+    {  
+        label: "clash_winner",
+        icon: "winner.png",
+        description: "<h3>Stone Love loses to Mighty Crown</h3>",
+        category: "clash_winner",
+        latitude:  18.302380604025146     , 
+        longitude: -77.31216430664062
+    },
+    {  
+        label: "clash_winner",
+        icon: "winner.png",
+        description: "<h3>Stone Love loses to Mighty Crown</h3>",
+        category: "clash_winner",
+        latitude:  18.042726987280457    , 
+        longitude: -77.11029052734375
+    },
+    {  
+        label: "clash_winner",
+        icon: "winner.png",
+        description: "<h3>Stone Love loses to Mighty Crown</h3>",
+        category: "clash_winner",
+        latitude:  17.899034816533145   , 
+        longitude: -76.95510864257812
+    },
+    {  
+        label: "clash_winner",
+        icon: "winner.png",
+        description: "<h3>Stone Love loses to Mighty Crown</h3>",
+        category: "clash_winner",
+        latitude:  17.899034816533145   , 
+        longitude: -76.95510864257812
+    },
+    {  
+        label: "clash_winner",
+        icon: "winner.png",
+        description: "<h3>Stone Love loses to Mighty Crown</h3>",
+        category: "clash_winner",
+        latitude:  17.79576579725599   , 
+        longitude: -77.19268798828125
+    },
+    {  
+        label: "clash_winner",
+        icon: "winner.png",
+        description: "<h3>Stone Love loses to Mighty Crown</h3>",
+        category: "clash_winner",
+        latitude: 18.260653356758375   , 
+        longitude: -76.86172485351562
+    },
+    {  
+        label: "clash_winner",
+        icon: "winner.png",
+        description: "<h3>Stone Love loses to Mighty Crown</h3>",
+        category: "clash_winner",
+        latitude: 18.140631722312712  , 
+        longitude: -76.585693359375
+    },
+    {  
+        label: "clash_winner",
+        icon: "winner.png",
+        description: "<h3>Stone Love loses to Mighty Crown</h3>",
+        category: "clash_winner",
+        latitude: 18.101476387775914 , 
+        longitude: -6.3824462890625
+    },
+    {  
+        label: "clash_winner",
+        icon: "winner.png",
+        description: "<h3>Stone Love loses to Mighty Crown</h3>",
+        category: "clash_winner",
         latitude: 17.929089201618645, 
         longitude: -76.5802001953125
     },
     {  
-        label: "clash_loser",
-        icon: "loser.png",
+        label: "clash_winner",
+        icon: "winner.png",
         description: "<h3>Stone Love loses to Mighty Crown</h3>",
-        category: "clash_loser",
+        category: "clash_winner",
         latitude: 18.187606552494625, 
         longitude: -77.64862060546875
     },
     {  
-        label: "clash_loser",
-        icon: "loser.png",
+        label: "clash_winner",
+        icon: "winner.png",
         description: "<h3>Real Man Sounds loses to Original Music</h3>",
-        category: "clash_loser",
+        category: "clash_winner",
         latitude: 18.359739156553683, 
         longitude: -78.18695068359375
     },
@@ -95,21 +171,21 @@ export const MAPMARKERS: MapMarker[] = [
         latitude: 18.375379094031825 , 
         longitude: -78.08807373046875
     },
-    -{  
+    {  
         label: "promotion",
         icon: "promotion.png",
-        description: "<h3>Free T-Shirts</h3>",
+        description: "<h3>3 Guiness For $500.00 JMD Fyah Side Jerk</h3>",
         category: "promotion",
-        latitude: 18.375379094031825 , 
-        longitude: -78.08807373046875
+        latitude: 17.970321 ,
+        longitude:  -77.352052
     },
     {  
         label: "promotion",
         icon: "promotion.png",
-        description: "<h3>Free T-Shirts</h3>",
+        description: "3 Guiness For $900.00 JMD At Ribbiz</br>"+ "<img src='../assets/share.png'>",
         category: "promotion",
-        latitude: 18.375379094031825 , 
-        longitude: -78.08807373046875
+        latitude: 18.029555,
+        longitude: -76.777686
     },
     {  
         label: "higlight",
